03bcbf7f0f2f867d17af1a04ac00157f0db4002a
shiqiang tau <sxt286@case.edu>
2013-06-02 19:32:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NativeSpeakers::Application.config.secret_token = '61676580a42d51e238986712917bc6237d2ca55bf2d4d4c7bb73d3fd24c070883a281174bf1881b3395791aeef459cd9baa92f964ad393185e485b21548b9fba'
